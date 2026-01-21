4339e4d7829b5b100985ca6b3564ea49ea23d3c9
zhaoxinhang <3285922090@qq.com>
2026-01-21 19:20:26 +0800

    轮播图的搜索框和指示灯导航

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -13,33 +13,100 @@ class HmSlider extends StatefulWidget {
 }
 
 class _HmSliderState extends State<HmSlider> {
-  
+  CarouselSliderController _controller = CarouselSliderController();// 控制轮播图跳转的控制器
+  int _currentIndex = 0;// 当前轮播图索引
+  // 轮播图
   Widget _getSlider() {
-    // 在fultter中获取屏幕宽度的方法
-    final double screenWideth = MediaQuery.of( context).size.width;
+    // 在flutter中获取屏幕宽度的方法
+    final double screenWidth = MediaQuery.of( context).size.width;
     // 返回轮播图插件
     // 根据数据渲染的不同的轮播图选项
     return CarouselSlider(
+      carouselController: _controller,// 绑定controller对象
       items: List.generate(widget.bannerList.length, (int index) {
         return Image.network(
           widget.bannerList[index].imgUrl,
           fit: BoxFit.cover,
-          width: screenWideth,
+          width: screenWidth,
         );
       }),
       options: CarouselOptions(
         viewportFraction: 1,
         autoPlay:  true,
         autoPlayInterval: Duration(seconds: 3),
+        onPageChanged: (int index, reason) {
+          _currentIndex = index;
+          setState(() {});
+        }
       )
     );
   }
+  // 搜索栏
+  Widget _getSearch() {
+    return Positioned(
+      top: 10,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+          child: Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            height: 50,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(0, 0, 0, 0.4),
+              borderRadius: BorderRadius.circular(25)
+            ),
+            child: Text(
+              "搜索...",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16
+              ),
+            ),
+          ),
+      )
+    );
+  }
+  // 返回指示灯导航部件
+  Widget _getDots() {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 10,
+      child: SizedBox(
+        height: 40,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,// 主轴居中
+          children: List.generate(widget.bannerList.length, (int index) {
+            return GestureDetector(
+              onTap: () {
+                _controller.animateToPage(index);
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                height: 6,
+                width: index == _currentIndex ? 40 : 20,
+                margin: EdgeInsets.symmetric(horizontal: 4),
+                decoration: BoxDecoration(
+                    color: index == _currentIndex ? Colors.white : Color.fromRGBO(0, 0, 0, 0.3),
+                    borderRadius: BorderRadius.circular(3)
+                ),
+              )
+            );
+          })
+        ),
+      )
+    );
+  }
+
   
   @override
   Widget build(BuildContext context) {
     // Stack -> 轮播图 搜索栏 指示灯导航
     return Stack(
-      children: [_getSlider()],
+      children: [_getSlider(), _getSearch(), _getDots()],
     );
     // return Container(
     //   height: 300,
